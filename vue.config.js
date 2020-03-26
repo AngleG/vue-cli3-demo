aadb0e7a5a1db45f3d1edb1d6dec6c55cd65259e
wxx <1464661791@qq.com>
2020-03-26 15:13:23 +0800

    引入阿里巴巴矢量图标

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path').resolve;
+module.exports = {
+  // css: {
+  //   requireModuleExtension: false
+  // },
+  // configureWebpack: config => {
+  //   config.resolve.extensions = ['.js', '.json', '.css', '.scss', '.vue', '.ts', '.tsx'];
+  //   const rules = [];
+  //   const plugins = [];
+  //   rules.push({
+  //     test: /\.tsx?$/,
+  //     exclude: /node_modules/
+  //   });
+  //   config.module.rules = [...config.module.rules, ...rules];
+  //   config.plugins = [...config.plugins, ...plugins];
+  // }
+};
