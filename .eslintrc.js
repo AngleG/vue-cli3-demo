fafdfb5742f3933000de4c6ba918ec82ba6949db
wxx <1464661791@qq.com>
2020-03-26 15:10:26 +0800

    关闭eslint在windows下的换行报错提示； 关闭类方法中必须使用this的规则；

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': [0 ,'error', 'windows'],
+    'class-methods-use-this': 'off',
   },
 };
