f3f5b93f354432d0b68a8ecc504341bf2a7c5328
wxx <1464661791@qq.com>
2020-04-09 13:29:38 +0800

    欢迎页完成

--- src/views/welcome/welcome.tsx
+++ src/views/welcome/welcome.tsx
@@ -1,8 +1,16 @@
 import { Vue, Component } from 'vue-property-decorator';
+import avatar from '../../assets/avatar.png';
+import './welcome.scss';
 
 @Component
 export default class Welcome extends Vue {
   render() {
-    return <div>欢迎回家</div>;
+    return <div class="welcome">
+      <div class="welcome-content">
+        <p class="welcome-title">WELCOME</p>
+        <p class="welcome-tips">欢迎来到呆哞中心</p>
+        <span class="avatar"><img src={avatar} width="160px"/></span>
+      </div>
+    </div>;
   }
 }
