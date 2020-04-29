36147cc9c9c998c4934fec50be7bf3cbe89defb9
wxx <1464661791@qq.com>
2020-04-29 11:39:44 +0800

    header组件新增修改密码弹窗

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,16 +3,42 @@ import { Component } from 'vue-property-decorator';
 import logo from '../../assets/logo.png';
 import avatar from '../../assets/avatar.png';
 
+const defaultFormData = {
+  oldPassword: null,
+  newPassword: null,
+  confirmPassword: null,
+};
+
 @Component
 export default class AppHeader extends Vue {
+  visible = false;
+
+  form = { ...defaultFormData };
+
   handleCommand(command: string) {
     console.log(command, '-=-=-=');
     if (command === 'signOut') {
       this.$router.push('/login');
+    } else if (command === 'changePassword') {
+      this.visible = true;
     }
   }
 
+  closeDialog() {
+    this.visible = false;
+    this.form = { ...defaultFormData };
+  }
+
+  submitHandle() {
+    this.$message({
+      message: '修改成功',
+      type: 'success',
+    });
+    this.visible = false;
+  }
+
   render() {
+    const { visible, form } = this.$data;
     return <el-header>
       <el-row>
         <el-col span={12} class="tl">
@@ -29,6 +55,30 @@ export default class AppHeader extends Vue {
           </el-dropdown>
         </el-col>
       </el-row>
+      <el-dialog
+        title="修改密码"
+        visible={visible}
+        {...{ on: { 'update:visible': (val: boolean) => { this.visible = val; } } }}
+        width="500px"
+        onClose={this.closeDialog}>
+        <div class="dialog-content">
+          <el-form model={form} label-position="right" label-width="80px">
+            <el-form-item label="旧密码:">
+              <el-input placeholder="请输入旧密码" value={form.oldPassword} onInput={(val: string) => { form.oldPassword = val; }}/>
+            </el-form-item>
+            <el-form-item label="新密码:">
+              <el-input placeholder="请输入新密码" value={form.newPassword} onInput={(val: string) => { form.newPassword = val; }}/>
+            </el-form-item>
+            <el-form-item label="确认密码:">
+              <el-input placeholder="请确认新密码" value={form.confirmPassword} onInput={(val: string) => { form.confirmPassword = val; }}/>
+            </el-form-item>
+          </el-form>
+        </div>
+        <span>
+          <el-button onClick={this.closeDialog}>取消</el-button>
+          <el-button type="primary" onClick={this.submitHandle}>确定</el-button>
+        </span>
+      </el-dialog>
     </el-header>;
   }
 }
